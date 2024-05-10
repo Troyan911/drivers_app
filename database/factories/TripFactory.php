fa4e2ac225e95f644ee931a3dd5594db5342f1e2
troyan911 <troyan911@gmail.com>
2024-05-10 18:11:32 +0300

    fix test

--- database/factories/TripFactory.php
+++ database/factories/TripFactory.php
@@ -18,7 +18,7 @@ class TripFactory extends Factory
     {
         $pickup = fake()->dateTimeBetween('-100 minutes', '-80 minutes')->format('Y-m-d H:i:s');
         $dropoff = fake()->dateTimeBetween('-79 minutes', '-50 minutes')->format('Y-m-d H:i:s');
-        $minutes = round((strtotime($dropoff) - strtotime($pickup)/60), 2);
+        $minutes = round((strtotime($dropoff) - strtotime($pickup))/60, 2);
 
         return [
             'id' => fake()->unique()->randomNumber(5),
